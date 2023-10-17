b7b78317bf37b679bc512e3c0724ccade21c5f03
arden82 <qinwen1018@gmail.com>
2023-10-17 20:11:58 +0800

    Initial commit

---
+++ src/main/java/com/tha103/artion/Artionhope/App.java
@@ -0,0 +1,13 @@
+package com.tha103.artion.Artionhope;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
