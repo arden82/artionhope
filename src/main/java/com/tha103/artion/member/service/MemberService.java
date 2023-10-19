f0b4d9ee2aaee7cd2e3aeefdb3f95577a2f83160
arden82 <qinwen1018@gmail.com>
2023-10-19 08:53:26 +0800

    修正filter、listener、util位置、修改xml內容、member mvc內容測試未完成版

--- src/main/java/com/tha103/artion/member/service/MemberService.java
+++ src/main/java/com/tha103/artion/member/service/MemberService.java
@@ -1,5 +1,10 @@
 package com.tha103.artion.member.service;
 
-public class MemberService {
+import com.tha103.artion.member.model.MemberVO;
 
+public interface MemberService {
+	int insert(MemberVO member);
+	int update(MemberVO member);
+	MemberVO getMember(Integer mem_id);
+	
 }
