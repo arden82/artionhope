02b0b6ec1140acc3c51b2e2e45a39f7be0326a19
Jason <jason987321z@gmail.com>
2023-10-20 02:00:38 +0800

    xun 10/20push

--- src/main/java/com/tha103/artion/merchOrderDetail/controller/MerchOrderDetailService.java
+++
@@ -1,5 +0,0 @@
-package com.tha103.artion.merchOrderDetail.controller;
-
-public class MerchOrderDetailService {
-
-}
--- src/main/java/com/tha103/artion/ticketOrder/controller/TicketOrderService.java
+++
@@ -1,5 +0,0 @@
-package com.tha103.artion.ticketOrder.controller;
-
-public class TicketOrderService {
-
-}
