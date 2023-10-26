0bc05b7feb408784b76c22d76464bb432352cee4
Lawrence <haohsuan861104@gmail.com>
2023-10-27 03:32:52 +0800

    完成活動留言新增並且可以顯示相對留言的圖片 10/27 03:32

--- src/main/java/com/tha103/artion/activityComment/controller/ActComPicServlet.java
+++ src/main/java/com/tha103/artion/activityComment/controller/ActComPicServlet.java
@@ -3,6 +3,7 @@ package com.tha103.artion.activityComment.controller;
 import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -20,43 +21,41 @@ import com.tha103.artion.activityComment.service.ActivityCommentService;
 public class ActComPicServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		 ServletOutputStream out = res.getOutputStream();
-		  res.setContentType("image/gif");
-		  ByteArrayInputStream byteInputStream =null;
-		  try {
-			  String str = req.getParameter("actComId");
-			  String actComCoverPicture=req.getParameter("actComCoverPicture");
-			  String actComPicture1=req.getParameter("actComPicture1");
-			  String actComPicture2=req.getParameter("actComPicture2");
-			  String actComPicture3=req.getParameter("actComPicture3");
-			  Integer actComId;
-			  actComId = Integer.valueOf(str);
-			  ActivityCommentService actComSvc = new ActivityCommentService();
-			  ActivityCommentVO ActivityCommentVO = actComSvc.getOneActCom(actComId);
-	
-		if(actComCoverPicture!=null) {
-			 byteInputStream = new ByteArrayInputStream(ActivityCommentVO.getActComCoverPicture());
-		}else if(actComPicture1!=null) {
-			 byteInputStream = new ByteArrayInputStream(ActivityCommentVO.getActComPicture1());
-		}else if(actComPicture2!=null){
-			 byteInputStream = new ByteArrayInputStream(ActivityCommentVO.getActComPicture2());
-		}else{
-			 byteInputStream = new ByteArrayInputStream(ActivityCommentVO.getActComPicture3());
-		}
-		   BufferedInputStream in = new BufferedInputStream(byteInputStream);
-		   byte[] buf = new byte[4 * 1024]; // 4K buffer
-		   int len;
-		   while ((len = in.read(buf)) != -1) {
-		    out.write(buf, 0, len);
-		   }
+	    ServletOutputStream out = res.getOutputStream();
+	    res.setContentType("image/gif");
+	    ByteArrayInputStream byteInputStream = null;
+
+	    try {
+	        String str = req.getParameter("actComId");
+	        String picNumber = req.getParameter("picNumber"); // 获取 picNumber 参数
+	        Integer actComId = Integer.valueOf(str);
+	        ActivityCommentService actComSvc = new ActivityCommentService();
+	        ActivityCommentVO activityCommentVO = actComSvc.getOneActCom(actComId);
+
+	        // 根据 picNumber 的不同值来确定返回哪张图片
+	        if ("cover".equals(picNumber)) {
+	            byteInputStream = new ByteArrayInputStream(activityCommentVO.getActComCoverPicture());
+	        } else if ("1".equals(picNumber)) {
+	            byteInputStream = new ByteArrayInputStream(activityCommentVO.getActComPicture1());
+	        } else if ("2".equals(picNumber)) {
+	            byteInputStream = new ByteArrayInputStream(activityCommentVO.getActComPicture2());
+	        } else if ("3".equals(picNumber)) {
+	            byteInputStream = new ByteArrayInputStream(activityCommentVO.getActComPicture3());
+	        }
+
+	        BufferedInputStream in = new BufferedInputStream(byteInputStream);
+	        byte[] buf = new byte[4 * 1024]; // 4K buffer
+	        int len;
+	        while ((len = in.read(buf)) != -1) {
+	            out.write(buf, 0, len);
+	        }
+	        in.close();
+	    } catch (Exception e) {
+		   InputStream in = getServletContext().getResourceAsStream("images/1.jpg");
+		   byte[] b = new byte[in.available()];
+		   in.read(b);
+		   out.write(b);
 		   in.close();
-		 
-		  } catch (Exception e) {
-//		   InputStream in = getServletContext().getResourceAsStream("images/1.jpg");
-//		   byte[] b = new byte[in.available()];
-//		   in.read(b);
-//		   out.write(b);
-//		   in.close();
 		  }
 
 		
--- src/main/java/com/tha103/artion/activityComment/controller/AddActComServlet.java
+++ src/main/java/com/tha103/artion/activityComment/controller/AddActComServlet.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.Collection;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -49,6 +50,7 @@ public class AddActComServlet extends HttpServlet {
 			ActivityVO activityVO = new ActivityVO();
 			activityVO.setActId(Integer.valueOf(actIdString));
 			activityCommentVO.setActivity(activityVO);
+			
 			/*************************** 2.開始新增資料 ***************************************/
 			byte[] actComCoverPicture = null;
 			byte[] actComPicture1 = null;
@@ -85,169 +87,24 @@ public class AddActComServlet extends HttpServlet {
 			
 			activityCommentVO.setActComPicture3(actComPicture3);
 			
-			
-			
-			
-//			Collection<Part> parts = req.getParts();
-//			for (Part part : parts) {
-//			 filename = part.getSubmittedFileName();
-//				partName = part.getName();
-//				if (filename!= null && filename.length()!=0 && part.getContentType()!=null) {
-//					
-//					if ("actComCoverPicture".equals(partName)) {
-//						InputStream is = part.getInputStream();
-//						ByteArrayOutputStream byteA = new ByteArrayOutputStream();
-//						     byte[] buf = new byte[4 * 1024];
-//						     int len;
-//						     while ((len = is.read(buf)) != -1) {
-//						      byteA.write(buf, 0, len);
-//						     }
-//						     actComCoverPicture = byteA.toByteArray();
-//						     byteA.close();
-//					}
-//					if ("actComPicture1".equals(partName)) {
-//						InputStream is = part.getInputStream();
-//						ByteArrayOutputStream byteA = new ByteArrayOutputStream();
-//						     byte[] buf = new byte[4 * 1024];
-//						     int len;
-//						     while ((len = is.read(buf)) != -1) {
-//						      byteA.write(buf, 0, len);
-//						     }
-//						     actComPicture1 = byteA.toByteArray();
-//						     byteA.close();
-//					}
-//					if ("actComPicture2".equals(partName)) {
-//						InputStream is = part.getInputStream();
-//						ByteArrayOutputStream byteA = new ByteArrayOutputStream();
-//						byte[] buf = new byte[4 * 1024];
-//						int len;
-//						while ((len = is.read(buf)) != -1) {
-//							byteA.write(buf, 0, len);
-//						}
-//						actComPicture2 = byteA.toByteArray();
-//						byteA.close();
-//					}
-//					
-//					if ("actComPicture3".equals(partName)) {
-//						InputStream is = part.getInputStream();
-//						ByteArrayOutputStream byteA = new ByteArrayOutputStream();
-//						byte[] buf = new byte[4 * 1024];
-//						int len;
-//						while ((len = is.read(buf)) != -1) {
-//							byteA.write(buf, 0, len);
-//						}
-//						actComPicture3 = byteA.toByteArray();
-//						byteA.close();
-//					}
-//					
-//					}
-//				}
 			MemberVO memberVO = new MemberVO();  //改成動態會員id
 			memberVO.setMemId(7001);
-			
 			activityCommentVO.setMember(memberVO);
+			
+			activityCommentVO.setActComLikeTimes(0);
+			activityCommentVO.setActComReportTimes(0);
+			
 			
 			ActivityCommentService actComSvc = new ActivityCommentService();
 			
 			activityCommentVO = actComSvc.addActCom(activityCommentVO);
 
 			/*************************** 3.新增完成,準備轉交(Send the Success view) ***********/
-//			url = "/activitycomment/listAllActCom.jsp";
-//			RequestDispatcher successView = req.getRequestDispatcher(url);
-//			successView.forward(req, res);
+			url = "/singleActivity.html?"+ actIdString;
+			RequestDispatcher successView = req.getRequestDispatcher(url);
+			successView.forward(req, res);
 
 		}
-		
-//		===========================================================
-//		PrintWriter out = res.getWriter();
-//		HttpSession session = req.getSession();
-//		
-////		接收參數
-//		String actComContent = req.getParameter("actComContent");
-//	    String actIdParam = req.getParameter("actId");
-//	    Part part1 = req.getPart("actComCoverPicture");
-//		Part part2 = req.getPart("actComPicture1");
-//		Part part3 = req.getPart("actComPicture2");
-//		Part part4 = req.getPart("actComPicture3");
-//	    byte[] actComCoverPicture = null;
-//		byte[] actComPicture1 = null;
-//		byte[] actComPicture2 = null;
-//		byte[] actComPicture3 = null;
-//		
-//		if (part1 != null && part1.getSize() > 0) {
-//			InputStream in = part1.getInputStream();
-//			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//			byte[] buf = new byte[4 * 1024]; // 4K buffer
-//			int len;
-//			while ((len = in.read(buf)) != -1) {
-//				byteArrayOutputStream.write(buf, 0, len);
-//			}
-//			actComCoverPicture = byteArrayOutputStream.toByteArray();
-//		}
-//		
-//		if (part2 != null && part1.getSize() > 0) {
-//			InputStream in = part1.getInputStream();
-//			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//			byte[] buf = new byte[4 * 1024]; // 4K buffer
-//			int len;
-//			while ((len = in.read(buf)) != -1) {
-//				byteArrayOutputStream.write(buf, 0, len);
-//			}
-//			actComPicture1 = byteArrayOutputStream.toByteArray();
-//		}
-//
-//		if (part3 != null && part1.getSize() > 0) {
-//			InputStream in = part1.getInputStream();
-//			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//			byte[] buf = new byte[4 * 1024]; // 4K buffer
-//			int len;
-//			while ((len = in.read(buf)) != -1) {
-//				byteArrayOutputStream.write(buf, 0, len);
-//			}
-//			actComPicture2 = byteArrayOutputStream.toByteArray();
-//		}
-//		
-//		if (part3 != null && part1.getSize() > 0) {
-//			InputStream in = part1.getInputStream();
-//			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//			byte[] buf = new byte[4 * 1024]; // 4K buffer
-//			int len;
-//			while ((len = in.read(buf)) != -1) {
-//				byteArrayOutputStream.write(buf, 0, len);
-//			}
-//			actComPicture3 = byteArrayOutputStream.toByteArray();
-//		}
-		
-		
-//	    try {
-//	        System.out.println("actComContent: " + actComContent);
-//	        
-//	        ActivityCommentService actComSvc = new ActivityCommentService();
-//	        ActivityCommentVO actComVO = new ActivityCommentVO();
-//	        MemberVO memberVO = new MemberVO();
-//        	memberVO.setMemId(7001); // 7001是在Servlet中硬编码的，你可以根据需要进行修改
-//        	actComVO.setMember(memberVO);
-//        	
-//	        actComVO.setActComContent(actComContent);
-//	        actComVO.setActComCoverPicture(actComCoverPicture);
-//	        actComVO.setActComPicture1(actComPicture1);
-//	        actComVO.setActComPicture2(actComPicture2);
-//	        actComVO.setActComPicture3(actComPicture3);
-//	        actComVO.setActComReportTimes(0); 
-//        	actComVO.setActComLikeTimes(0); 
-//        	actComVO.setActComStatus(true);
-//	        
-//        	actComSvc.addActCom(actComVO);
-//        	System.out.println("傳送資料庫成功");
-//	      
-//        	Gson gson = new Gson();
-//			String json = gson.toJson(res);
-//			out.print(json);
-//			out.flush();
-//	        
-//	    } catch (Exception e) {
-//	        e.printStackTrace();
-//	    }
 	}
 }
 
